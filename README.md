dbc14d71a60275f0ff19ac2f420eb3425ae6bf78
ozgunawesome <11738322+ozgunawesome@users.noreply.github.com>
2019-05-29 02:43:54 -0700

    wrapped connect function instead of listening for event

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ Otherwise the Raspberry Pi will half-assedly power the Ender 3 with only LCD wor
 
 **WARNING** This will power down all 4 USB ports and even the Ethernet port!
 
-**TODO** doesnt work very well yet, serial port needs to be set to Auto and it takes 2 clicks on Connect to 
-connect to the printer because the TTY device disappears when the USB power is cut off.
-
 ## Setup
 
 Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
@@ -23,7 +20,5 @@ No configuration required.
 **TODO** give at least some configurability to the end user.
 
 ## Mandatory warnings
-
-**THIS IS NOT PRODUCTION READY!!!!** Your house might go up in flames at any given moment.
 
 #### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- octoprint_usbpowercontrol/__init__.py
+++ octoprint_usbpowercontrol/__init__.py
@@ -1,30 +1,45 @@
 # coding=utf-8
 from __future__ import absolute_import
-import octoprint.plugin
 import os
+import time
+import octoprint.plugin
 
-class USBPowerControlPlugin(octoprint.plugin.EventHandlerPlugin):
-
+class USBPowerControlPlugin(octoprint.plugin.SettingsPlugin, octoprint.plugin.EventHandlerPlugin, octoprint.plugin.StartupPlugin):
   def __init__(self):
-    self.on_command = "echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind; sleep 1"
+    self.on_command = "echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind"
     self.off_command = "echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind"
 
-  def on_after_startup(self):
-    self._logger.info("USB Power Control loaded")
-    pass
+  def get_settings_defaults(self):
+    return dict(
+      enable_usb_on_connect=True,
+      enable_usb_on_startup=True,
+      delay_after_usb_enable=2.0
+    )
+
+  def on_startup(self, *_):
+    self._usb_delay = self._settings.get_float(["delay_after_usb_enable"])
+    if self._settings.get_boolean(["enable_usb_on_startup"]):
+      self._logger.info("Enabling raspberry pi USB power...")
+      os.system(self.on_command)
+
+    if self._settings.get_boolean(["enable_usb_on_startup"]):
+      self._logger.info("Patching connect function to enable USB power on connection")
+      original_connect=self._printer.connect
+      
+      def wrapped_connect(*args, **kwargs):
+        self._logger.info(
+            "Enabling USB power via shell command and sleeping %f seconds." % self._usb_delay)
+        os.system(self.on_command)
+        time.sleep(self._usb_delay)
+        original_connect(*args, **kwargs)
+      
+      self._printer.connect = wrapped_connect
+      return
 
   def on_event(self, event, payload):
     if event == "Disconnected":
       self._logger.info("Printer disconnected, turning off USB power")
       os.system(self.off_command)
       return
-    elif event == "Connecting":
-      self._logger.info("Printer will connect, turn on USB power")
-      os.system(self.on_command)
-      return
 
-
-__plugin_name__ = "USB Power Control"
-__plugin_version__ = "0.0.1"
-__plugin_description__ = "Cut Raspberry Pi 3B+ USB power on disconnect. Re-enable on connect"
 __plugin_implementation__ = USBPowerControlPlugin()
